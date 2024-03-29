bbedc93c8b61568c1e63c63790b792afd5579f66
Vinffs <vin.demiranda@gmail.com>
2024-03-29 17:34:47 +0100

    created methods to get account data + deposit

--- src/org/learning/bank/Bank.java
+++ src/org/learning/bank/Bank.java
@@ -1,5 +1,6 @@
 package org.learning.bank;
 
+import java.math.BigDecimal;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,21 +9,39 @@ public class Bank {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Welcome to Your Bank!");
-        System.out.println("Would you like to create a new bank account? (Y/N)");
-        String response = scan.nextLine().toUpperCase();
 
-        if(response.equals("Y")) {
-            System.out.println("Very Well! Let's get started!");
-            System.out.println("Give us your full name: ");
-            String fullName = scan.nextLine();
+        System.out.print("Please give us your full name: ");
+        String fullName = scan.nextLine();
 
-            Random randomGen = new Random();
-            int randomNum = randomGen.nextInt(1001);
-            String accountNumber = String.format("%025d", randomNum);
+        BankAccount user = new BankAccount(fullName);
 
-            System.out.println("Dear " + fullName + ", your bank account has been created! Account number: IT" + accountNumber);
+        System.out.println("Dear " + user.getName() + ", your bank account has been created! Account number: IT" + user.getAccountNumber());
+        System.out.println("Your balance is: €" + user.getBalance());
 
+
+        boolean flag = false;
+
+        while(!flag) {
+            System.out.println("Would you like to make a deposit? (Y/N) ");
+            String deposit = scan.nextLine().toUpperCase();
+            if(deposit.equals("Y")) {
+                System.out.print("How much would you like to deposit? ");
+                BigDecimal depositValue = new BigDecimal(scan.nextLine());
+
+                System.out.println("Great! Your balance is now: € " + user.deposit(depositValue));
+            }
+
+            System.out.println("Would you like to make a withdraw? (Y/N) ");
+            String withdraw = scan.nextLine().toUpperCase();
+            if(withdraw.equals("Y")) {
+                System.out.print("How much would you like to withdraw?");
+                double withdrawValue = Integer.parseInt(scan.nextLine());
+            }
+            System.out.println("Would you like to exit the menu? (Y/N) ");
+            String exit = scan.nextLine().toUpperCase();
+            flag = exit.equals("Y");
         }
+
 
         scan.close();
     }
--- src/org/learning/bank/BankAccount.java
+++ src/org/learning/bank/BankAccount.java
@@ -1,15 +1,32 @@
 package org.learning.bank;
 
+import java.math.BigDecimal;
+import java.util.Random;
+
 public class BankAccount {
 
     // ATTRIBUTES
-    /*
-    account number
-    holder name
-    balance
-     */
+
+    private String accountNumber;
+
+    private String holderName;
+
+    private BigDecimal balance;
+
 
     // CONSTRUCTORS
+
+    public BankAccount(String holderName) {
+
+        Random randomGen = new Random();
+        int randomNum = randomGen.nextInt(1001);
+
+        this.accountNumber = String.format("%025d", randomNum);
+        this.holderName = holderName;
+        this.balance = BigDecimal.valueOf(0);
+
+    }
+
 
     // METHODS
 
@@ -19,4 +36,20 @@ public class BankAccount {
     metodi per ritornare le informazioni del conto e il saldo
 
      */
+
+    public String getName() {
+        return this.holderName;
+    }
+
+    public String getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public BigDecimal getBalance() {
+        return this.balance;
+    }
+
+    public BigDecimal deposit(BigDecimal value) {
+        return this.balance.add(value);
+    }
 }
