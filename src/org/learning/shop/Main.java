b364e3e8ca40e90c30f2bd78f3f2b4adf16db26b
Vinffs <vin.demiranda@gmail.com>
2024-03-29 15:26:45 +0100

    Testing Constructor and generating random number and treating price with BigDecimal

---
+++ src/org/learning/shop/Main.java
@@ -0,0 +1,13 @@
+package org.learning.shop;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Product prova = new Product("Prova", "Questa è una prova", 29.00);
+        System.out.println(Arrays.asList(prova));
+
+    }
+}
+
---
+++ src/org/learning/shop/Product.java
@@ -0,0 +1,38 @@
+package org.learning.shop;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Random;
+
+public class Product {
+
+    // ATTRIBUTES
+    private int code;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private final double vat = 0.23;
+
+    // CONSTRUCTORS
+
+    public Product(String name, String description, double price) {
+        // generate random number
+        Random randomGenerator = new Random();
+        int randomCode = randomGenerator.nextInt(99_999_999);
+
+        // treat price as a number with 2 decimals
+        BigDecimal correctPrice = new BigDecimal(price).setScale(2, RoundingMode.HALF_UP);
+
+        this.code = randomCode;
+        this.name = name;
+        this.description = description;
+        this.price = correctPrice;
+    }
+
+
+    // METHODS
+
+
+
+
+}
