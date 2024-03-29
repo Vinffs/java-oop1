fc6b32ae8b405381b26a0890bd5e507a39812046
Vinffs <vin.demiranda@gmail.com>
2024-03-29 16:03:09 +0100

    Created two products in Main using methods and constructor of Product

--- src/org/learning/shop/Main.java
+++ src/org/learning/shop/Main.java
@@ -8,12 +8,23 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        Product prova = new Product("Prova", "Questa è una prova", 29.00);
+        System.out.println();
 
+        Product iphone = new Product("Iphone 99 Pro", "New Chip M10 Ultra Pro Mega Hyper...", 200_000);
 
-        System.out.println(prova.netPrice());
-        System.out.println(prova.grossPrice());
-        System.out.println(prova.fullName());
+        System.out.println("Check Out Our New Product!");
+        System.out.println("Net Price: € " + iphone.netPrice());
+        System.out.println("Gross Price: € " + iphone.grossPrice());
+        System.out.println("Full Product name: " + iphone.fullName());
+
+        System.out.println();
+
+        Product samsung = new Product("Samsung XYZ", "Better than the Iphone for sure", 1249.99);
+
+        System.out.println("Check Out Our New Product!");
+        System.out.println("Net Price: € " + samsung.netPrice());
+        System.out.println("Gross Price: € " + samsung.grossPrice());
+        System.out.println("Full Product name: " + samsung.fullName());
 
     }
 }
